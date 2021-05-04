bb1455c97655df09f61f13dc9df9639354ed27ba
Russel Musasa <Russel.Muamba@rawbank.cd>
2021-05-04 12:37:16 +0100

    Modules & transpile for ie11

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+// var webpack = require('webpack');
+
+module.exports = {
+    mode: "production",
+    devtool: 'source-map',
+    entry: './js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.bundle.js'
+    },
+    target: ['web', 'es5'],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    stats: {
+        colors: true
+    }
+};
