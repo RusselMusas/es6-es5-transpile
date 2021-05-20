e87c5fb0b7aa12957245bcd89c5df5d23c6cd416
Russel Musasa <Russel.Muamba@rawbank.cd>
2021-05-20 12:39:54 +0100

    Setup css minimizer plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var HtmlWebpackSkipAssetsPlugin = require("html-webpack-skip-assets-plugin").HtmlWebpackSkipAssetsPlugin;
+var CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     devtool: 'source-map',
     entry: {
         main: ['babel-polyfill', './js/main.js', './css/styles.css'],
@@ -53,6 +54,14 @@ module.exports = {
               use: [MiniCssExtractPlugin.loader, "css-loader"]
             }
         ]
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
     },
     stats: {
         colors: true
